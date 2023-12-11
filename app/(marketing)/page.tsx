d8027cea6c58a164c088fa181120b4cb01837bb0
Kachi <onlinehassle1234@gmail.com>
2023-12-11 22:52:40 +0100

    button

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -56,7 +56,7 @@ const MarketingPage = () => {
       <Dialog>
         <DialogTrigger asChild>
           <Button className="mt-6 animate-pulse" size="lg">
-            Click me to watch a video to see how this app works.
+            Watch a video to see how this app works.
           </Button>
         </DialogTrigger>
         <DialogContent className="sm:max-w-[525px] flex items-center flex-col">
